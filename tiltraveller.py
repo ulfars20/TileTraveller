afaff633d7d67da97c0cc83236ecd16ea81d9e38
ulfars20 <ulfars20@ru.is>
2020-09-17 09:42:34 +0000

    hæ

---
+++ tiltraveller.py
@@ -0,0 +1,18 @@
+#def north(x, y)
+#def south(x, y)
+#def west(x, y)
+    # Check if error?
+#def east(x, y)
+
+# Set x and y equal to 1 (start)
+# Check which direction is available
+    # If y > x you can move east?
+    # If x is 1 then west is unavailable
+    # If x is 3 then east is unavailable
+    # If y is 1 then south is unavailable
+    # If y is 3 then north is anavailable
+# Prompt for a direction
+# Error if wrong direction is called
+# Call a function based on the input
+
+# If you are in 3, 1 then print Victory
